64b313c2e08d35cff9c47b4f9ee1138b26b379dd
Kevin Pak <kedpak@yahoo.com>
2018-01-16 15:31:30 -0800

    implement google maps api to fetch location of address

--- app.js
+++ app.js
@@ -1,8 +1,23 @@
 const req = require('request');
+const yargs = require('yargs');
+const argv = yargs.options({
+  a: {
+    demand: true,
+    alias: 'address',
+    describe: 'Address to fetch weather',
+    string: true
+  }
+}).help().alias('help', 'h').argv;
+
+console.log(argv);
+
+console.log(encodeURIComponent(argv));
 
 req({
-  url: 'https://maps.googleapis.com/maps/api/geocode/json?address=1315%20gateview%20ave%20california',
+  url: `https://maps.googleapis.com/maps/api/geocode/json?address=${encodeURIComponent(argv.a)}`,
   json: true
 }, (error, response, body) => {
-  console.log(body);
+  console.log(`Address: ${body.results[0].formatted_address}`);
+  console.log(`Latitude: ${body.results[0].geometry.location.lat}`);
+  console.log(`Longitude: ${body.results[0].geometry.location.lng}`);
 });
