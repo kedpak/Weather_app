83cb8c3068478cf0b9e938b4d73716ecf03e2817
Kevin Pak <kedpak@yahoo.com>
2018-01-16 15:38:45 -0800

    add error conditions and messages

--- app.js
+++ app.js
@@ -17,6 +17,11 @@ req({
   url: `https://maps.googleapis.com/maps/api/geocode/json?address=${encodeURIComponent(argv.a)}`,
   json: true
 }, (error, response, body) => {
+  if (error) {
+    console.log('Cannot connect to servers.');
+  } else if (body.status === 'ZERO_RESULTS'){
+    console.log('Unable to find the adddress!');
+  }
   console.log(`Address: ${body.results[0].formatted_address}`);
   console.log(`Latitude: ${body.results[0].geometry.location.lat}`);
   console.log(`Longitude: ${body.results[0].geometry.location.lng}`);
