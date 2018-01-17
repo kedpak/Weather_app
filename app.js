d4c3e91f9cc477692371f5f6f09e327c94ef9316
Kevin Pak <kedpak@yahoo.com>
2018-01-16 22:04:25 -0800

    use callbacks to grab data of address and place in object

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
-const req = require('request');
 const yargs = require('yargs');
+const geocode = require('./geocode/geocode.js');
+
+
 const argv = yargs.options({
   a: {
     demand: true,
@@ -9,22 +11,11 @@ const argv = yargs.options({
   }
 }).help().alias('help', 'h').argv;
 
-console.log(argv);
 
-console.log(encodeURIComponent(argv));
-
-req({
-  url: `https://maps.googleapis.com/maps/api/geocode/json?address=${encodeURIComponent(argv.a)}`,
-  json: true
-}, (error, response, body) => {
-  if (error) {
-    console.log('Cannot connect to servers.');
-  } else if (body.status === 'ZERO_RESULTS'){
-    console.log('Unable to find the adddress!');
-  } else if (body.status === 'OK') {
-    console.log(`Address: ${body.results[0].formatted_address}`);
-    console.log(`Latitude: ${body.results[0].geometry.location.lat}`);
-    console.log(`Longitude: ${body.results[0].geometry.location.lng}`);
+geocode.geocodeAddress(argv.address, (errorMessage, results) => {
+  if (errorMessage) {
+    console.log(errorMessage);
+  } else {
+    console.log(JSON.stringify(results, undefined, 4));
   }
-
 });
